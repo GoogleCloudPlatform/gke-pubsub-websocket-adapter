8e0b1078292a9629b8ae96f862fedf85b8c14a5a
Salvatore Sferrazza <sferrazza@google.com>
2020-11-12 00:07:55 -0500

    use pulltop from npm vs. bundling

--- container/exec.sh
+++ container/exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Server"
-while true; do node /project/pulltop/pulltop.js $@ > output.json ; done &
+while true; do pulltop $@ > output.json ; done &
 websocketd --port=8080 --devconsole tail -f output.json
